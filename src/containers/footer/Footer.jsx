5b8c00c1db1b8c97b1e5a7868a61e43cc957f69d
Timotej V <133594886+timotejvas@users.noreply.github.com>
2023-08-01 15:17:07 +0200

    Update Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="gpt3__footer-copyright">
-        <p>@2021 GPT-3. All rights reserved.</p>
+        <p>@2021 GPT-3. All rights reserved. Made by Timotej Vaško</p>
       </div>
     </div>
   )
